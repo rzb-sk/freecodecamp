b606ccd5edff27504a97241b042263ecb6edbdf4
rzb-sk <riyazshaik4me@gmail.com>
2025-03-04 19:36:20 +0530

    py43 - chapter 5

---
+++ 1-py4e/chapter-5-loops-and-iterations/for-loop.py
@@ -0,0 +1,11 @@
+# With numbers
+for i in [5,4,3,2,1,0]:
+    print(i)
+print('Blastoff!')
+print('Firecracker EXPLOSION!!', '\n')
+
+# with strings
+friends = ['Jasmin', 'Gowhar', 'Layah']
+for friend in friends:
+    print('Happy New Year:', friend)
+print('Done!')
