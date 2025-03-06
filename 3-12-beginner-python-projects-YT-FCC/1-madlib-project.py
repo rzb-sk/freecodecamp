eb80d0d8c4c227a704da5ad83fa8b2334644a8c0
rzb-sk <riyazshaik4me@gmail.com>
2025-03-06 21:58:46 +0530

    3rd course - 1st commit

---
+++ 3-12-beginner-python-projects-YT-FCC/1-madlib-project.py
@@ -0,0 +1,15 @@
+# String concatenation is used here.
+
+name = input("Name: ")
+country = input("Country: ")
+capital = input("Capital: ")
+age = input("Age: ")
+hobby1 = input("Hobby: ")
+hobby2 = input("Hobby: ")
+hobby3 = input("Hobby: ")
+
+madlib = f"Hi my name is {name}. I am from {country} and the capital of \
+my country is {capital}. I am {age} years old. My hobbies are {hobby1}, {hobby2} and \
+{hobby3}."
+
+print(madlib)
